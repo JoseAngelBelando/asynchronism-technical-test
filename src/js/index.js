cb28118f5b6cab307534ec43f82bc3868ede810e
joseAngel33 <163841769+joseAngel33@users.noreply.github.com>
2024-07-16 12:24:04 +0200

    +update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,23 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+// defino una funcion asincrona
+const fetchAdvice = async () => {
+  const adviceElement = document.getElementById('advice');
+  //   muestra loading mientras carga
+  adviceElement.textContent = 'Loading...';
+  // inio try para mostrar errores
+  try {
+    // realizamos solicitud
+    const response = await fetch('https://api.adviceslip.com/advice');
+    // convierte la respuesta en json
+    const data = await response.json();
+    // actualiza el contenido de el elemento advice
+    adviceElement.textContent = data.slip.advice;
+    // si hay cualquier error
+  } catch (error) {
+    // muestra un texto de error
+    adviceElement.textContent = 'Failed to fetch advice. Please try again.';
+  }
+};
+// metodo de escucha:al hacer click se ejecuta la función fetchAdvice
+document.getElementById('generate-advice').addEventListener('click', fetchAdvice);
